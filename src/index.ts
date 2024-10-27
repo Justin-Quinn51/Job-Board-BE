746dc17850f152ac4b1b07ad0f352f451e52cdcf
Justin Quinn <jpq3366@gmail.com>
2024-10-26 22:42:28 -0400

    correct port env variable to PORT

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello from Express with TS!");
 });
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`running on port ${process.env.PORT}`);
 });
