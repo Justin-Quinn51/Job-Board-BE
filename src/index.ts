f097b54052676a6eeacb9b7be2b70f6eae6309ca
Justin Quinn <jpq3366@gmail.com>
2024-11-12 18:03:25 -0500

    Import routes from jobs router and mount them at specified path

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { type Request, type Response } from "express";
 import "dotenv/config";
+import jobRoutes from "./routes/jobRoutes.ts";
 
 const app = express();
 app.use(express.json());
+
+app.use("/jobs", jobRoutes);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`running on port ${process.env.PORT}`);
