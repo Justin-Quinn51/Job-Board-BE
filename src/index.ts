0ca3462e423d2d2b4e68bcda9d560b2197b8c0d3
Justin Quinn <jpq3366@gmail.com>
2024-11-22 11:42:31 -0500

    Import custom error handler and add to middleware to catch all errors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express, { type Request, type Response } from "express";
 import "dotenv/config";
 import jobRoutes from "./routes/jobRoutes.ts";
+import { errorHandler } from "./errors/errorHandler.ts";
 
 const app = express();
 app.use(express.json());
 
 app.use("/jobs", jobRoutes);
+app.use(errorHandler);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`running on port ${process.env.PORT}`);
